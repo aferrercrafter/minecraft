0a900d4a217db47692ec2128e8a6d3bbaee4dfcb
Arnaldo Corzo Ferrer <aferrercrafter@gmail.com>
2020-09-13 10:51:05 -0300

    added custom tool ruby sword

--- forge-minecraft-1.15.2/tutorial-mod/src/main/java/com/aferrercrafter/tutorial/util/RegistryHandler.java
+++ forge-minecraft-1.15.2/tutorial-mod/src/main/java/com/aferrercrafter/tutorial/util/RegistryHandler.java
@@ -4,8 +4,10 @@ import com.aferrercrafter.tutorial.Tutorial;
 import com.aferrercrafter.tutorial.blocks.BlockItemBase;
 import com.aferrercrafter.tutorial.blocks.RubyBlock;
 import com.aferrercrafter.tutorial.items.ItemBase;
+import com.aferrercrafter.tutorial.tools.ModItemTier;
 import net.minecraft.block.Block;
 import net.minecraft.item.Item;
+import net.minecraft.item.SwordItem;
 import net.minecraftforge.fml.RegistryObject;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import net.minecraftforge.registries.DeferredRegister;
@@ -22,6 +24,10 @@ public class RegistryHandler {
 
     // Items
     public static final RegistryObject<Item> RUBY = ITEMS.register("ruby", ItemBase::new);
+
+    // Tools
+    public static final RegistryObject<SwordItem> RUBY_SWORD = ITEMS.register("ruby_sword", () ->
+            new SwordItem(ModItemTier.RUBY, 2, -2.4f, new Item.Properties().group(Tutorial.TAB)));
 
     // Blocks
     public static final RegistryObject<Block> RUBY_BLOCK = BLOCKS.register("ruby_block", RubyBlock::new);

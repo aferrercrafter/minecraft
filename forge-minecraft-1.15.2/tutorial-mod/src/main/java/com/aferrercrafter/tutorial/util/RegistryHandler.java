7fee89017a23adc5dd08c870ceaaa30efd31e56c
Arnaldo Corzo Ferrer <aferrercrafter@gmail.com>
2020-09-17 19:37:21 -0300

    adding block and block item

--- forge-minecraft-1.15.2/tutorial-mod/src/main/java/com/aferrercrafter/tutorial/util/RegistryHandler.java
+++ forge-minecraft-1.15.2/tutorial-mod/src/main/java/com/aferrercrafter/tutorial/util/RegistryHandler.java
@@ -1,7 +1,10 @@
 package com.aferrercrafter.tutorial.util;
 
 import com.aferrercrafter.tutorial.Tutorial;
+import com.aferrercrafter.tutorial.blocks.BlockItemBase;
+import com.aferrercrafter.tutorial.blocks.RubyBlock;
 import com.aferrercrafter.tutorial.items.ItemBase;
+import net.minecraft.block.Block;
 import net.minecraft.item.Item;
 import net.minecraftforge.fml.RegistryObject;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
@@ -10,6 +13,7 @@ import net.minecraftforge.registries.ForgeRegistries;
 
 public class RegistryHandler {
     public static final DeferredRegister<Item> ITEMS = new DeferredRegister<>(ForgeRegistries.ITEMS, Tutorial.MOD_ID);
+    public static final DeferredRegister<Block> BLOCKS = new DeferredRegister<>(ForgeRegistries.BLOCKS, Tutorial.MOD_ID);
 
     public static void init(){
         ITEMS.register(FMLJavaModLoadingContext.get().getModEventBus());
@@ -17,4 +21,10 @@ public class RegistryHandler {
 
     // Items
     public static final RegistryObject<Item> RUBY = ITEMS.register("ruby", ItemBase::new);
+
+    // Blocks
+    public static final RegistryObject<Block> RUBY_BLOCK = BLOCKS.register("ruby_block", RubyBlock::new);
+
+    // Blocks Items
+    public static final RegistryObject<Item> RUBY_BLOCK_ITEM = ITEMS.register("ruby_block", () -> new BlockItemBase(RUBY_BLOCK.get()));
 }

4f3df53f10d602656fa95eed19125001047356bd
Arnaldo Corzo Ferrer <aferrercrafter@gmail.com>
2020-09-21 11:07:39 -0300

    added pickaxe, hoe, axe, and shovel custom items ruby

--- forge-minecraft-1.15.2/tutorial-mod/src/main/java/com/aferrercrafter/tutorial/util/RegistryHandler.java
+++ forge-minecraft-1.15.2/tutorial-mod/src/main/java/com/aferrercrafter/tutorial/util/RegistryHandler.java
@@ -6,8 +6,7 @@ import com.aferrercrafter.tutorial.blocks.RubyBlock;
 import com.aferrercrafter.tutorial.items.ItemBase;
 import com.aferrercrafter.tutorial.tools.ModItemTier;
 import net.minecraft.block.Block;
-import net.minecraft.item.Item;
-import net.minecraft.item.SwordItem;
+import net.minecraft.item.*;
 import net.minecraftforge.fml.RegistryObject;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import net.minecraftforge.registries.DeferredRegister;
@@ -26,8 +25,11 @@ public class RegistryHandler {
     public static final RegistryObject<Item> RUBY = ITEMS.register("ruby", ItemBase::new);
 
     // Tools
-    public static final RegistryObject<SwordItem> RUBY_SWORD = ITEMS.register("ruby_sword", () ->
-            new SwordItem(ModItemTier.RUBY, 2, -2.4f, new Item.Properties().group(Tutorial.TAB)));
+    public static final RegistryObject<SwordItem> RUBY_SWORD = ITEMS.register("ruby_sword", () -> new SwordItem(ModItemTier.RUBY, 2, -2.4f, new Item.Properties().group(Tutorial.TAB)));
+    public static final RegistryObject<PickaxeItem> RUBY_PICKAXE = ITEMS.register("ruby_pickaxe", () -> new PickaxeItem(ModItemTier.RUBY, 0, -2.8f, new Item.Properties().group(Tutorial.TAB)));
+    public static final RegistryObject<ShovelItem> RUBY_SHOVEL = ITEMS.register("ruby_shovel", () -> new ShovelItem(ModItemTier.RUBY, 0, -3.0f, new Item.Properties().group(Tutorial.TAB)));
+    public static final RegistryObject<AxeItem> RUBY_AXE = ITEMS.register("ruby_axe", () -> new AxeItem(ModItemTier.RUBY, 5, -3.1f, new Item.Properties().group(Tutorial.TAB)));
+    public static final RegistryObject<HoeItem> RUBY_HOE = ITEMS.register("ruby_hoe", () -> new HoeItem(ModItemTier.RUBY, -1-0f, new Item.Properties().group(Tutorial.TAB)));
 
     // Blocks
     public static final RegistryObject<Block> RUBY_BLOCK = BLOCKS.register("ruby_block", RubyBlock::new);

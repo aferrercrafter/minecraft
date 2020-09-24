124fb6055e6f1bb21c1841f4d84267aa0c548fae
Arnaldo Corzo Ferrer <aferrercrafter@gmail.com>
2020-09-24 17:23:16 -0300

    Adding tag logic

---
+++ forge-minecraft-1.15.2/tutorial-mod/src/main/java/com/aferrercrafter/tutorial/Tags.java
@@ -0,0 +1,12 @@
+package com.aferrercrafter.tutorial;
+
+import com.aferrercrafter.tutorial.util.RegistryHandler;
+import net.minecraft.item.Item;
+import net.minecraft.tags.ItemTags;
+import net.minecraft.util.ResourceLocation;
+
+public class Tags {
+    ResourceLocation myTagId = new ResourceLocation("forge", "gems/ruby");
+    Item testItem = RegistryHandler.RUBY.get();
+    boolean isInGroup = ItemTags.getCollection().getOrCreate(myTagId).contains(testItem);
+}

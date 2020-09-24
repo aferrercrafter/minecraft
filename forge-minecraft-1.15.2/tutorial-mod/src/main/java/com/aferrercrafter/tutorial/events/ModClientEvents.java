8e16c00c303b22fab6c85defbb72e9e72c708cb4
Arnaldo Corzo Ferrer <aferrercrafter@gmail.com>
2020-09-24 19:12:22 -0300

    adding onDamageSheep event

--- forge-minecraft-1.15.2/tutorial-mod/src/main/java/com/aferrercrafter/tutorial/events/ModClientEvents.java
+++ forge-minecraft-1.15.2/tutorial-mod/src/main/java/com/aferrercrafter/tutorial/events/ModClientEvents.java
@@ -3,10 +3,17 @@ package com.aferrercrafter.tutorial.events;
 import com.aferrercrafter.tutorial.Tutorial;
 import com.aferrercrafter.tutorial.util.RegistryHandler;
 import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.passive.SheepEntity;
+import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.Items;
+import net.minecraft.potion.EffectInstance;
+import net.minecraft.potion.Effects;
+import net.minecraft.util.text.StringTextComponent;
+import net.minecraft.util.text.TextFormatting;
 import net.minecraft.world.World;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.event.entity.living.LivingEvent;
+import net.minecraftforge.event.entity.player.AttackEntityEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 
@@ -21,6 +28,22 @@ public class ModClientEvents {
             World world = player.getEntityWorld();
             world.setBlockState(player.getPosition().add(0,-1,0), RegistryHandler.RUBY_BLOCK.get().getDefaultState());
         }
+    }
+
+    @SubscribeEvent
+    public static void onDamageSheep(AttackEntityEvent event){
+        if(event.getEntityLiving().getHeldItemMainhand().getItem() == RegistryHandler.POISON_APPLE.get())
+            if(event.getTarget().isAlive()){
+                LivingEntity target = (LivingEntity) event.getTarget();
+                if(target instanceof SheepEntity){
+                    PlayerEntity player = event.getPlayer();
+                    target.addPotionEffect(new EffectInstance(Effects.POISON, 10 * 20));
+                    target.setGlowing(true);
+
+                    if(!event.getPlayer().getEntityWorld().isRemote)
+                        player.sendMessage(new StringTextComponent(TextFormatting.RED + "That sheep isn't filling so well.."));
+                }
+            }
     }
 
 }
